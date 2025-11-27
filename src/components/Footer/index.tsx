31a32af6e1b7ae6c474c3919d958ae854ab96f8b
Emilia Hernandez <emilia.b.hernandez@gmail.com>
2025-11-26 17:13:26 -0800

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -80,7 +80,7 @@ export const Footer = () => {
               target="_blank"
             >
               <Image
-                src="/candid_silver.svg"
+                src="Candid_Silver.svg"
                 alt="Candid"
                 width={500}
                 height={500}
