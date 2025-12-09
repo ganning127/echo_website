4660b8e22bc1fd83e0aa7a7ce5d0ae23a17e626d
Emilia Hernandez <emilia.b.hernandez@gmail.com>
2025-12-08 21:48:59 -0800

    Update FAQ.tsx

--- src/components/Echo Explorers FAQ/FAQ.tsx
+++ src/components/Echo Explorers FAQ/FAQ.tsx
@@ -51,7 +51,7 @@ export default function FAQSection() {
     {
       question: "When does the program take place?",
       answer:
-        "Classes are held on Mondays from 3:00–4:00 pm for 10 weeks. Although IPA after-school programs begin the week of January 19, this class will start on January 26 due to the MLK Jr. holiday. There will be no class on February 16 (Presidents’ Day) and March 16 (Spring Break).",
+        "Classes are held on Wednesdays from 3:00–4:00 pm for 12 weeks. Although IPA after-school programs begin the week of January 19, this class will start on January 26 due to the MLK Jr. holiday. There will be no class on February 16 (Presidents’ Day) and March 16 (Spring Break).",
     },
     {
       question: "Is the heart health fair open to the public?",
