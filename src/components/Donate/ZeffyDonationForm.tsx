0dfcc54cc762ff5f7db8be27f6c9f82854557f74
Emilia Hernandez <emilia.b.hernandez@gmail.com>
2025-12-09 09:00:24 -0800

    Donate ScrollBar
    Updated for Safari compatibility

--- src/components/Donate/ZeffyDonationForm.tsx
+++ src/components/Donate/ZeffyDonationForm.tsx
@@ -2,30 +2,13 @@
 
 export default function ZeffyDonationForm() {
   return (
-    <div
-      style={{
-        position: "relative",
-        // Allow iframe scroll to be visible
-        overflow: "visible",
-        height: "500px",
-        width: "100%",
-      }}
-    >
+    <div className="relative h-[500px] w-full overflow-y-scroll custom-scrollbar">
       <iframe
         title="Donation form powered by Zeffy"
         src="https://www.zeffy.com/embed/donation-form/donate-to-change-lives-5034"
         allow="payment-request"
         data-cookiecategory="necessary"
-        style={{
-          position: "absolute",
-          border: 0,
-          inset: 0,
-          width: "100%",
-          height: "100%",
-          // Force scrollbars to appear on mobile/tablet Safari
-          overflowY: "auto",
-          WebkitOverflowScrolling: "touch",
-        }}
+        className="absolute inset-0 w-full h-full border-0"
       />
     </div>
   );
