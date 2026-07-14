5d7bed13d1dc13e213f3ab96e75b0da61d51c878
Emilia Hernandez <emilia.b.hernandez@gmail.com>
2026-07-13 21:30:02 -0400

    KlaviyoForm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { GoogleAnalytics } from "@next/third-parties/google";
 import { lato } from "./fonts";
 import Silktide from "@/components/Tracking/Silktide";
 import GoogleConsent from "@/components/Tracking/GoogleConsent";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: {
@@ -29,6 +30,54 @@ export default function RootLayout({
 
         {/* Google Analytics (always loaded, consent-controlled) */}
         <GoogleAnalytics gaId="G-S3G9CP9H0H" />
+
+       <Script
+  id="klaviyo-library"
+  src="https://static.klaviyo.com/onsite/js/TiMahB/klaviyo.js?company_id=TiMahB"
+  strategy="afterInteractive"
+/>
+
+{/* Initialize Klaviyo */}
+<Script
+  id="klaviyo-init"
+  strategy="afterInteractive"
+>
+  {`
+    !function(){
+      if(!window.klaviyo){
+        window._klOnsite=window._klOnsite||[];
+        try{
+          window.klaviyo=new Proxy({},{
+            get:function(n,i){
+              return "push"===i
+                ? function(){
+                    var n;
+                    (n=window._klOnsite).push.apply(n,arguments)
+                  }
+                : function(){
+                    for(var n=arguments.length,o=new Array(n),w=0;w<n;w++)o[w]=arguments[w];
+                    var t="function"==typeof o[o.length-1]?o.pop():void 0;
+                    var e=new Promise(function(resolve){
+                      window._klOnsite.push([i].concat(o,[function(i){
+                        t&&t(i);
+                        resolve(i);
+                      }]));
+                    });
+                    return e;
+                  };
+            }
+          });
+        }catch(n){
+          window.klaviyo=window.klaviyo||[];
+          window.klaviyo.push=function(){
+            var n;
+            (n=window._klOnsite).push.apply(n,arguments);
+          };
+        }
+      }
+    }();
+  `}
+</Script>
       </body>
     </html>
   );
---
+++ src/components/KlaviyoForms/wishing-well.tsx
@@ -0,0 +1,9 @@
+export function WishingWell() {
+  return (
+    <section className="py-12">
+      <div className="mx-auto max-w-3xl px-6">
+        <div className="klaviyo-form-Vrin4G"></div>
+      </div>
+    </section>
+  );
+}
