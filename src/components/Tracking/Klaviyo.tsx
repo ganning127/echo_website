8cb978ad39a086f522c6f3a393bee491c31328dd
Emilia Hernandez <emilia.b.hernandez@gmail.com>
2026-07-15 18:24:24 -0400

    klaviyo tracking

---
+++ src/components/Tracking/Klaviyo.tsx
@@ -0,0 +1,21 @@
+// components/Tracking/Klaviyo.tsx
+import Script from "next/script";
+
+export default function Klaviyo() {
+  return (
+    <>
+      <Script
+        id="klaviyo-library"
+        async
+        type="text/javascript"
+        src={`https://static.klaviyo.com/onsite/js/TiMahB/klaviyo.js?company_id=${process.env.NEXT_PUBLIC_KLAVIYO_COMPANY_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="klaviyo-init" strategy="afterInteractive">
+        {`
+          !function(){if(!window.klaviyo){window._klOnsite=window._klOnsite||[];try{window.klaviyo=new Proxy({},{get:function(n,i){return"push"===i?function(){var n;(n=window._klOnsite).push.apply(n,arguments)}:function(){for(var n=arguments.length,o=new Array(n),w=0;w<n;w++)o[w]=arguments[w];var t="function"==typeof o[o.length-1]?o.pop():void 0,e=new Promise((function(n){window._klOnsite.push([i].concat(o,[function(i){t&&t(i),n(i)}]))}));return e}}})}catch(n){window.klaviyo=window.klaviyo||[],window.klaviyo.push=function(){var n;(n=window._klOnsite).push.apply(n,arguments)}}}}();
+        `}
+      </Script>
+    </>
+  );
+}
